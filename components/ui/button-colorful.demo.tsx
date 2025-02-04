bad1e34cf586fd16d3f9eea9222f25beffe2f2b1
w3bsuki <twstlxyz@gmail.com>
2025-02-04 06:40:23 -0800

    fix: add proper demo for ButtonColorful component

--- components/ui/button-colorful.demo.tsx
+++ components/ui/button-colorful.demo.tsx
@@ -1,9 +1,25 @@
+'use client'
+
 import { ButtonColorful } from "@/components/ui/button-colorful";
 import { Button } from "@/components/ui/button";
 import { LucideIcon } from "lucide-react"
 
 function ButtonDemo() {
-    return <ButtonColorful />
+    return (
+        <div className="flex flex-col sm:flex-row gap-4">
+            <ButtonColorful size="sm">
+                Small Button
+            </ButtonColorful>
+            
+            <ButtonColorful>
+                Default Button
+            </ButtonColorful>
+            
+            <ButtonColorful size="lg">
+                Large Button
+            </ButtonColorful>
+        </div>
+    )
 }
 
 export { ButtonDemo }
