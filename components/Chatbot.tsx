c45320c271d7d48e980e412bc1bde3363d1daf19
w3bsuki <twstlxyz@gmail.com>
2025-02-04 21:16:49 -0800

    feat: add modern dropdown navigation component

--- components/Chatbot.tsx
+++ components/Chatbot.tsx
@@ -1,71 +1,25 @@
 "use client"
 
 import React from "react"
-import { CanvasRevealEffect } from "@/components/ui/canvas-reveal-effect"
 import { DIcons } from "dicons"
-import { AnimatePresence, motion } from "framer-motion"
-
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { ScrollArea } from "@/components/ui/scroll-area"
 
 export function Chatbot() {
-  const [hovered, setHovered] = React.useState(false)
-
   return (
-    <div className="">
-      <div
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
-        className="relative mx-auto w-full items-center justify-center overflow-hidden"
-      >
+    <div className="w-full">
+      <div className="relative mx-auto w-full items-center justify-center">
         <div className="relative flex w-full items-center justify-center p-4">
-          <AnimatePresence>
-            <div className="tracking-tightest flex select-none flex-col py-2 text-center text-3xl font-extrabold leading-none md:flex-col md:text-8xl lg:flex-row"></div>
-            {hovered && (
-              <motion.div
-                initial={{ opacity: 1 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 1 }}
-                className="absolute inset-0 h-full w-full object-cover"
-              >
-                <CanvasRevealEffect
-                  containerClassName="bg-transparent opacity-30 dark:opacity-50"
-                  variant="red"
-                />
-              </motion.div>
-            )}
-          </AnimatePresence>
           <div className="z-20 w-full">
             <ScrollArea className="h-[360px] w-full overflow-auto p-1">
               <div className="px-6">
                 <div className="relative flex h-full w-full justify-center text-center">
                   <h1 className="flex select-none py-2 text-center text-2xl font-extrabold leading-none tracking-tight md:text-2xl lg:text-4xl">
-                    <span
-                      data-content="AI."
-                      className="before:animate-gradient-background-1 relative before:absolute before:bottom-4 before:left-0 before:top-0 before:z-0 before:w-full before:px-2 before:content-[attr(data-content)] sm:before:top-0"
-                    >
-                      <span className="from-gradient-1-start to-gradient-1-end animate-gradient-foreground-1 bg-gradient-to-r bg-clip-text px-2 text-transparent">
-                        AI.
-                      </span>
-                    </span>
-                    <span
-                      data-content="Chat."
-                      className="before:animate-gradient-background-2 relative before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:w-full before:px-2 before:content-[attr(data-content)] sm:before:top-0"
-                    >
-                      <span className="from-gradient-2-start to-gradient-2-end animate-gradient-foreground-2 bg-gradient-to-r bg-clip-text px-2 text-transparent">
-                        Chat.
-                      </span>
-                    </span>
-                    <span
-                      data-content="Experience."
-                      className="before:animate-gradient-background-3 relative before:absolute before:bottom-1 before:left-0 before:top-0 before:z-0 before:w-full before:px-2 before:content-[attr(data-content)] sm:before:top-0"
-                    >
-                      <span className="from-gradient-3-start to-gradient-3-end animate-gradient-foreground-3 bg-gradient-to-r bg-clip-text px-2 text-transparent">
-                        Experience.
-                      </span>
-                    </span>
+                    <span className="px-2">AI.</span>
+                    <span className="px-2">Chat.</span>
+                    <span className="px-2">Experience.</span>
                   </h1>
                 </div>
                 <p className="md:text-md lg:text-md mx-auto mt-1 text-center text-xs text-primary/60 md:max-w-2xl">
@@ -83,7 +37,7 @@ export function Chatbot() {
               </div>
             </ScrollArea>
 
-            <div className="relative  mt-2 w-full">
+            <div className="relative mt-2 w-full">
               <form>
                 <div className="">
                   <Input className="pl-12" placeholder="Ask something with AI" />
--- components/ui/dropdown-navigation.tsx
+++ components/ui/dropdown-navigation.tsx
@@ -5,7 +5,101 @@ import React from "react"
 import { motion, AnimatePresence } from "framer-motion"
 import { ChevronDown } from "lucide-react"
 
-interface NavItem {
+export function DropdownNavigation({ navItems }: { navItems: Props[] }) {
+  const [openMenu, setOpenMenu] = React.useState<string | null>(null)
+
+  const handleHover = (menuLabel: string | null) => {
+    setOpenMenu(menuLabel)
+  }
+
+  const [isHover, setIsHover] = useState<number | null>(null)
+  return (
+    <main className="relative w-full min-h-screen flex items-start md:items-center justify-center px-4 py-10">
+      <div className="relative gap-5 flex flex-col items-center justify-center">
+        <ul className="relative flex items-center space-x-0">
+          {navItems.map((navItem) => (
+            <li
+              key={navItem.label}
+              className="relative"
+              onMouseEnter={() => handleHover(navItem.label)}
+              onMouseLeave={() => handleHover(null)}
+            >
+              <button
+                className="text-sm py-1.5 px-4 flex cursor-pointer group transition-colors duration-300 items-center justify-center gap-1 text-muted-foreground hover:text-foreground relative"
+                onMouseEnter={() => setIsHover(navItem.id)}
+                onMouseLeave={() => setIsHover(null)}
+              >
+                <span>{navItem.label}</span>
+                {navItem.subMenus && (
+                  <ChevronDown
+                    className={`h-4 w-4 group-hover:rotate-180 duration-300 transition-transform
+                      ${openMenu === navItem.label ? "rotate-180" : ""}`}
+                  />
+                )}
+                {(isHover === navItem.id || openMenu === navItem.label) && (
+                  <motion.div
+                    layoutId="hover-bg"
+                    className="absolute inset-0 size-full bg-primary/10"
+                    style={{ borderRadius: 99 }}
+                  />
+                )}
+              </button>
+
+              <AnimatePresence>
+                {openMenu === navItem.label && navItem.subMenus && (
+                  <div className="w-auto absolute left-0 top-full pt-2">
+                    <motion.div
+                      className="bg-background border border-border p-4 w-max"
+                      style={{ borderRadius: 16 }}
+                      layoutId="menu"
+                    >
+                      <div className="w-fit shrink-0 flex space-x-9 overflow-hidden">
+                        {navItem.subMenus.map((sub) => (
+                          <motion.div layout className="w-full" key={sub.title}>
+                            <h3 className="mb-4 text-sm font-medium capitalize text-muted-foreground">
+                              {sub.title}
+                            </h3>
+                            <ul className="space-y-6">
+                              {sub.items.map((item) => {
+                                const Icon = item.icon
+                                return (
+                                  <li key={item.label}>
+                                    <a
+                                      href="#"
+                                      className="flex items-start space-x-3 group"
+                                    >
+                                      <div className="border border-border text-foreground rounded-md flex items-center justify-center size-9 shrink-0 group-hover:bg-accent group-hover:text-accent-foreground transition-colors duration-300">
+                                        <Icon className="h-5 w-5 flex-none" />
+                                      </div>
+                                      <div className="leading-5 w-max">
+                                        <p className="text-sm font-medium text-foreground shrink-0">
+                                          {item.label}
+                                        </p>
+                                        <p className="text-xs text-muted-foreground shrink-0 group-hover:text-foreground transition-colors duration-300">
+                                          {item.description}
+                                        </p>
+                                      </div>
+                                    </a>
+                                  </li>
+                                )
+                              })}
+                            </ul>
+                          </motion.div>
+                        ))}
+                      </div>
+                    </motion.div>
+                  </div>
+                )}
+              </AnimatePresence>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
+  )
+}
+
+type Props = {
   id: number
   label: string
   subMenus?: {
@@ -17,101 +111,4 @@ interface NavItem {
     }[]
   }[]
   link?: string
-}
-
-interface Props {
-  navItems: NavItem[]
-}
-
-export function DropdownNavigation({ navItems }: Props) {
-  const [openMenu, setOpenMenu] = React.useState<string | null>(null)
-  const [isHover, setIsHover] = useState<number | null>(null)
-
-  const handleHover = (menuLabel: string | null) => {
-    setOpenMenu(menuLabel)
-  }
-
-  return (
-    <div className="relative gap-5 flex flex-col items-center justify-center">
-      <ul className="relative flex items-center space-x-2">
-        {navItems.map((navItem) => (
-          <li
-            key={navItem.label}
-            className="relative"
-            onMouseEnter={() => handleHover(navItem.label)}
-            onMouseLeave={() => handleHover(null)}
-          >
-            <button
-              className="text-base py-2 px-5 flex cursor-pointer group transition-colors duration-300 items-center justify-center gap-1.5 text-white/70 hover:text-white relative"
-              onMouseEnter={() => setIsHover(navItem.id)}
-              onMouseLeave={() => setIsHover(null)}
-            >
-              <span>{navItem.label}</span>
-              {navItem.subMenus && (
-                <ChevronDown
-                  className={`h-4 w-4 group-hover:rotate-180 duration-300 transition-transform
-                    ${openMenu === navItem.label ? "rotate-180" : ""}`}
-                />
-              )}
-              {(isHover === navItem.id || openMenu === navItem.label) && (
-                <motion.div
-                  layoutId="hover-bg"
-                  className="absolute inset-0 size-full bg-white/5 rounded-full"
-                />
-              )}
-            </button>
-
-            <AnimatePresence>
-              {openMenu === navItem.label && navItem.subMenus && (
-                <div className="w-auto absolute left-1/2 -translate-x-1/2 top-full pt-2">
-                  <motion.div
-                    className="bg-black/95 backdrop-blur-sm border border-white/10 p-6 w-max rounded-2xl shadow-xl"
-                    initial={{ opacity: 0, y: 10 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    exit={{ opacity: 0, y: 10 }}
-                    transition={{ duration: 0.2 }}
-                  >
-                    <div className="w-fit shrink-0 flex space-x-12 overflow-hidden">
-                      {navItem.subMenus.map((sub) => (
-                        <motion.div layout className="w-full" key={sub.title}>
-                          <h3 className="mb-4 text-sm font-medium capitalize text-white/50">
-                            {sub.title}
-                          </h3>
-                          <ul className="space-y-4">
-                            {sub.items.map((item) => {
-                              const Icon = item.icon
-                              return (
-                                <li key={item.label}>
-                                  <a
-                                    href="#"
-                                    className="flex items-start space-x-3 group"
-                                  >
-                                    <div className="border border-white/10 text-white/70 rounded-lg flex items-center justify-center size-10 shrink-0 group-hover:bg-white/5 group-hover:text-white transition-colors duration-300">
-                                      <Icon className="h-5 w-5 flex-none" />
-                                    </div>
-                                    <div className="leading-5">
-                                      <p className="text-sm font-medium text-white/90 group-hover:text-white">
-                                        {item.label}
-                                      </p>
-                                      <p className="text-xs text-white/50 group-hover:text-white/70 transition-colors duration-300">
-                                        {item.description}
-                                      </p>
-                                    </div>
-                                  </a>
-                                </li>
-                              )
-                            })}
-                          </ul>
-                        </motion.div>
-                      ))}
-                    </div>
-                  </motion.div>
-                </div>
-              )}
-            </AnimatePresence>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
 } 
