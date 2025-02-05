1ff3e3899689f473873c14337e4c0ff5654e2071
w3bsuki <twstlxyz@gmail.com>
2025-02-05 01:09:59 -0800

    fix: update theme provider with correct type definitions

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,8 +2,18 @@
 
 import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { type ThemeProviderProps } from 'next-themes/dist/types'
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+interface ThemeProviderProps {
+  children: React.ReactNode
+  attribute?: string
+  defaultTheme?: string
+  enableSystem?: boolean
+  disableTransitionOnChange?: boolean
+}
+
+export function ThemeProvider({ 
+  children,
+  ...props
+}: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
