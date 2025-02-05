6f07079af90f3ccf81d6bb89c6de5c2f2259f21b
w3bsuki <twstlxyz@gmail.com>
2025-02-05 01:14:26 -0800

    fix: use correct types from next-themes package

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,19 +1,11 @@
 'use client'
 
 import * as React from 'react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps as NextThemeProviderProps } from 'next-themes'
 
-interface ThemeProviderProps {
-  children: React.ReactNode
-  attribute?: string
-  defaultTheme?: string
-  enableSystem?: boolean
-  disableTransitionOnChange?: boolean
-}
-
-export function ThemeProvider({ 
+export function ThemeProvider({
   children,
   ...props
-}: ThemeProviderProps) {
+}: NextThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
